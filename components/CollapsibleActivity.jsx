59e195ed388122269d1ed240eff6c350fffb0617
eternal <mariovparaschiv@gmail.com>
2021-10-16 12:36:16 +0100

    slight cleanup & add option to only collapse first activity (#6)

--- components/CollapsibleActivity.jsx
+++ components/CollapsibleActivity.jsx
@@ -1,35 +1,46 @@
-const { React, getModuleByDisplayName } = require('powercord/webpack')
-const { Icon } = require('powercord/components')
-const UserActivity = getModuleByDisplayName('UserActivity', false)
+const { React, getModuleByDisplayName, i18n: { Messages } } = require('powercord/webpack');
+const { findInReactTree } = require('powercord/util');
+const { Icon } = require('powercord/components');
+
+const UserActivity = getModuleByDisplayName('UserActivity', false);
 
 module.exports = class CollapsibleActivity extends React.PureComponent {
-    constructor(props) {
-        super(props)
-        this.state = { opened: props.defaultOpened }
-    }
+   constructor(props) {
+      super(props);
+      this.state = {
+         opened: props.defaultOpened
+      };
+   }
 
-    render() {
-        if (this.props.i > 0) delete this.props.streamingGuild
+   render() {
+      if (this.props.i > 0) delete this.props.streamingGuild;
 
-        const header = UserActivity.prototype.renderHeader.apply({ props: this.props, activity: this.props.activity })
-        if (!Array.isArray(header?.props?.children)) return null
-        header.props.children.push(<Icon name={`ArrowDrop${this.state.opened ? 'Up' : 'Down'}`} style={{ marginLeft: 'auto' }} />)
-        header.props.onClick = () => this.setState({ opened: !this.state.opened })
-        header.props.className += ' allactivities-collapsibleheader'
+      const header = UserActivity.prototype.renderHeader.apply({ props: this.props, activity: this.props.activity });
+      if (!Array.isArray(header?.props?.children)) return null;
 
-        if (!this.state.opened) {
-            if (!header.props.children.find(c => c?.includes && c.includes(this.props.activity.name))) {
-                if (header.props.children.find(c => c?.includes && c.includes('Playing a game'))) {
-                    header.props.children.splice(header.props.children.length - 3, 1, `Playing ${this.props.activity.name}`)
-                } else {
-                    header.props.children.splice(header.props.children.length - 2, 0, ` ${this.props.activity.name}`)
-                }
-            }
-            return header
-        }
-        return <>
-            {header}
-            <UserActivity __saa={true} {...this.props} hideHeader={true} />
-        </>
-    }
-}
+      header.props.children.push(
+         <Icon
+            name={`ArrowDrop${this.state.opened ? 'Up' : 'Down'}`}
+            className='allactivities-header-arrow'
+         />
+      );
+
+      header.props.onClick = () => this.setState({ opened: !this.state.opened });
+      header.props.className += ' allactivities-collapsibleheader';
+
+      if (!this.state.opened) {
+         const text = findInReactTree(header, t => t?.type?.displayName == 'OverflowTooltip');
+
+         if (text && text?.props?.children == Messages.USER_ACTIVITY_HEADER_PLAYING) {
+            text.props.children = `Playing ${this.props.activity.name}`;
+         }
+
+         return header;
+      }
+
+      return <>
+         {header}
+         <UserActivity __saa={true} {...this.props} hideHeader={true} />
+      </>;
+   }
+};
---
+++ index.jsx
@@ -0,0 +1,167 @@
+// Internals
+const { getModule, getModuleByDisplayName, i18n: { Messages }, React } = require('powercord/webpack');
+const { Button, Tooltip } = require('powercord/components');
+const { inject, uninject } = require('powercord/injector');
+const { findInReactTree } = require('powercord/util');
+const { Plugin } = require('powercord/entities');
+
+// Components
+const CollapsibleActivity = require('./components/CollapsibleActivity');
+const Settings = require('./components/Settings');
+
+// Modules
+const Arrow = getModule(m => m.displayName === 'Arrow' && !m.prototype?.render, false);
+const { getGame, getGameByName } = getModule(['getGame', 'getGameByName'], false);
+const UserActivity = getModuleByDisplayName('UserActivity', false);
+const { getActivities } = getModule(['getActivities'], false);
+const classes = getModule(['iconButtonSize'], false);
+
+// Utility
+let temp;
+const filterActivities = (a, i) => {
+   if (i === 0) temp = [];
+   if (temp.includes(a.application_id || a.name)) return false;
+   temp.push(a.application_id || a.name);
+   return a.type !== 4;
+};
+
+const _getGame = a => getGame(a.application_id) || getGameByName(a.name);
+
+module.exports = class ShowAllActivities extends Plugin {
+   async startPlugin() {
+      this.loadStylesheet('style.css');
+
+      powercord.api.settings.registerSettings(this.entityID, {
+         category: this.entityID,
+         label: 'Show All Activities',
+         render: Settings
+      });
+
+      inject('show-all-activities-pre', UserActivity.prototype, 'render', function (args) {
+         if (this.props.__saa || this.props.type === 'Profile') return args;
+
+         const activities = getActivities(this.props.user.id).filter(filterActivities);
+         if (!activities || !activities.length) return args;
+
+         if (!this.state) {
+            this.state = {
+               activity: activities.indexOf(this.props.activity)
+            };
+         } else {
+            const activity = activities[this.state.activity];
+            if (!activity) return args;
+
+            this.props.activity = activity;
+            this.props.game = _getGame(activity);
+
+            if (this.state.activity > 0 && this.props.streamingGuild) {
+               this.props._streamingGuild = this.props.streamingGuild;
+               delete this.props.streamingGuild;
+            } else if (this.state.activity === 0 && this.props._streamingGuild) {
+               this.props.streamingGuild = this.props._streamingGuild;
+            }
+         }
+
+         return args;
+      }, true);
+
+      const _this = this;
+      inject('show-all-activities', UserActivity.prototype, 'render', function (_, res) {
+         if (this.props.type === 'Profile') return res;
+         if (this.props.__saa || this.state?.activity > 0) {
+            const actions = findInReactTree(res, c => c && c.onOpenConnections);
+            if (actions) {
+               actions.activity = this.props.activity;
+               delete actions.applicationStream;
+            }
+
+            const icon = findInReactTree(res, c => c && c.className && c.game);
+            if (icon) icon.game = this.props.game;
+         }
+
+         if (this.props.__saa) {
+            res = res.props.children;
+            return res;
+         }
+
+         const activities = getActivities(this.props.user.id).filter(filterActivities);
+         const collapsible = _this.settings.get('collapsibleActivities');
+         if (
+            !res?.props?.children ||
+            !activities ||
+            !activities.length ||
+            collapsible && activities.length <= 1
+         ) return res;
+
+         if (collapsible) {
+            const opened = _this.settings.get('autoOpen', true);
+            const openFirst = _this.settings.get('autoOpenFirst', true);
+
+            res.props.children = activities.map((a, i) => {
+               return (
+                  <CollapsibleActivity
+                     {...this.props}
+                     activity={a}
+                     game={_getGame(a)}
+                     defaultOpened={(openFirst ? i == 0 : opened)}
+                     i={i}
+                  />
+               );
+            });
+
+            return res;
+         }
+
+         const { children } = res.props.children[1].props;
+         const marginClass = this.props.activity.details || this.props.activity.state ?
+            `allactivities-margin${this.props.activity.type === 1 && this.props.source === 'Profile Modal' ? '2' : ''}`
+            : '';
+
+         if (this.state.activity != 0) {
+            children.unshift(
+               <Tooltip
+                  className={['allactivities-left', marginClass].join(' ')}
+                  text={Messages.PAGINATION_PREVIOUS}
+               >
+                  <Button
+                     className={classes.iconButtonSize}
+                     size={Button.Sizes.MIN}
+                     color={Button.Colors.WHITE}
+                     look={Button.Looks.OUTLINED}
+                     onClick={() => this.setState({ activity: this.state.activity - 1 })}
+                  >
+                     <Arrow direction='LEFT' />
+                  </Button>
+               </Tooltip>
+            );
+         }
+
+         if (this.state.activity < activities.length - 1) {
+            children.push(
+               <Tooltip
+                  className={['allactivities-left', marginClass].join(' ')}
+                  text={Messages.NEXT}
+               >
+                  <Button
+                     className={classes.iconButtonSize}
+                     size={Button.Sizes.MIN}
+                     color={Button.Colors.WHITE}
+                     look={Button.Looks.OUTLINED}
+                     onClick={() => this.setState({ activity: this.state.activity + 1 })}
+                  >
+                     <Arrow direction='RIGHT' />
+                  </Button>
+               </Tooltip>
+            );
+         }
+
+         return res;
+      });
+   };
+
+   pluginWillUnload() {
+      powercord.api.settings.unregisterSettings(this.entityID);
+      uninject('show-all-activities-pre');
+      uninject('show-all-activities');
+   }
+};
